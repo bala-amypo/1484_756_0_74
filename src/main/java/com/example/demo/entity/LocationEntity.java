b48aa22976fd73b238dd8f0f6003b6701fe8d576
bala-amypo <bala@amypo.in>
2025-12-17 09:52:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/LocationEntity.java
+++ src/main/java/com/example/demo/entity/LocationEntity.java
@@ -1,5 +1,4 @@
 package com.example.demo.entity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +7,7 @@ import jakarta.persistence.Id;
 @Entity
 public class LocationEntity {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Double latitude;
@@ -19,40 +18,31 @@ public class LocationEntity {
         this.latitude = latitude;
         this.longitude = longitude;
     }
-
+    public LocationEntity() {
+    }
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Double getLatitude() {
         return latitude;
     }
-
     public void setLatitude(Double latitude) {
         this.latitude = latitude;
     }
-
     public Double getLongitude() {
         return longitude;
     }
-
     public void setLongitude(Double longitude) {
         this.longitude = longitude;
-    }
-
-    public LocationEntity() {
     }
 
 
